6ed14c0f3d927ea97f8bd1f311207aa7c21832aa
Chris Taylor <ctaylor534@gmail.com>
2022-09-22 22:19:58 -0700

    Adds solution to reverse_string, added challenge of in place

---
+++ reverse_string.rb
@@ -0,0 +1,22 @@
+# Write a function that returns the reverse of a given string.
+
+# Input: “abcde”
+# Output: “edcba”
+
+### CHALLENGE: Reverse in place
+
+def reverse_string(string)
+  i = 0
+  j = string.length - 1
+  while i != j
+    temp = string[i]
+    string[i] = string[j]
+    string[j] = temp
+    i += 1
+    j -= 1
+  end
+
+  return string
+end
+
+p reverse_string("abcde")

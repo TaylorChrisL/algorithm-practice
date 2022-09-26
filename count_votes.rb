ff3f50d989142f3593507276c08b5b104b77ed0e
Chris Taylor <ctaylor534@gmail.com>
2022-09-26 11:15:51 -0700

    adds solution to count_votes

---
+++ count_votes.rb
@@ -0,0 +1,22 @@
+# Given an array of strings, return a hash that provides of a count of how many times each string occurs.
+
+# Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+# Output: {"Dewey" => 6, "Truman" => 5}
+
+# Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
+def vote_counter(array)
+  output = {}
+  array.each do |vote|
+    if output[vote]
+      output[vote] += 1
+    else
+      output[vote] = 1
+    end
+  end
+
+  return output
+end
+
+p vote_counter(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
